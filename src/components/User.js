d84f4d9a017be4f282a0ca5efc70d076c69f2ef0
yusuftask08 <52704572+yusuftask08@users.noreply.github.com>
2022-01-17 21:17:25 +0300

    Döngülerde key prop'u

--- src/components/User.js
+++ src/components/User.js
@@ -1,14 +1,18 @@
 function User(props) {
     console.log('props :>> ', props);
     return (
-        <div>
+        <>
             <h1>
                 {
                     props.isLoggedIn ? `${props.name} ${props.surname}` : 'giriş yapmadınız.'
                 }
             </h1>
-
-        </div>
+            {props.friends.map((f, i) => (
+                <div key={f.id}>
+                    {f.div}- {f.name}
+                </div>
+            ))}
+        </>
     )
 }
 export default User
